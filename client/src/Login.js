53fbcb9fb794421232156d259d6949d456b452f2
connor <connorriley@outlook.com>
2023-10-17 16:22:27 -0400

    fixed routing

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './LoginValidation'
 import validation from './LoginValidation';
 function Login() {
@@ -10,47 +10,51 @@ function Login() {
     })
     const [errors, setErrors] = useState([])
     const handleInput = (event) => {
-        setValues(prev =>({...prev, [event.target.name]: [event.target.value]}))
+        setValues(prev => ({ ...prev, [event.target.name]: [event.target.value] }))
     }
     const [email, setEmail] = useState();
     const [password, setPassword] = useState()
     const nav = useNavigate(); //nav the goat fr
     axios.defaults.withCredentials = true;
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrors(validation(values))
-        axios.post('https://mern5-api.vercel.app/login', {values})
-        .then(result => {
-            console.log(result)
-            if(result.data == "Success"){
-                nav('/home')
-            }
-        })
-        .catch(err => console.log(err))
+        axios.post('https://mern5-api.vercel.app/login', { values })
+            .then(result => {
+                console.log(result)
+                if (result.data == "Success") {
+                    nav('/home')
+                }
+            })
+            .catch(err => console.log(err))
     }
-    
 
-    return(
-        <div className = "b-flex justify-content-center align-items-center bg-blue">
-            <div className = "bg-white p-3 rounded w-25">
-			<h2>Sign-in</h2>
-                <form action = "" onSubmit = {handleSubmit}>
-                    <div className = "mb-3">
-                        <label htmlFor = "email">Email<strong/></label>
-                        <input type = "email" placeholder = "Enter Email" name='email'
-                        onChange={handleInput} className = 'from-control rounded-0'/>
+
+    return (
+        <div className="b-flex justify-content-center align-items-center bg-blue">
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Sign-in</h2>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="email">Email<strong /></label>
+                        <input type="email" placeholder="Enter Email" name='email'
+                            onChange={handleInput} className='from-control rounded-0' />
                         {errors.email && <span className='text-danger'> {errors.email}</span>}
 
                     </div>
-                    <div className = "mb-3">
-                        <label htmlFor = "password">Password<strong/></label>
-                        <input type = "password" placeholder = "Enter Password" name ='password'
-                        onChange={handleInput} className = 'from-control rounded-0'/>
+                    <div className="mb-3">
+                        <label htmlFor="password">Password<strong /></label>
+                        <input type="password" placeholder="Enter Password" name='password'
+                            onChange={handleInput} className='from-control rounded-0' />
                         {errors.password && <span className='text-danger'> {errors.password}</span>}
                     </div>
-                    <button type = 'submit' className = "btn btn-success">Login</button>
-                    <Link to = "/Signup" className = "btn btn-default border">Signup</Link>
+                    <button type='submit' className="btn btn-success">Login</button>
+                    <Link to="/">
+                        <button className='btn'>
+                            Register
+                        </button>
+                    </Link>
                 </form>
             </div>
         </div>
