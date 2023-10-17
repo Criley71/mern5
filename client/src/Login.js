bd748b803cff7344ba9fafa7687a91e8c0fc2e1f
connor <connorriley@outlook.com>
2023-10-17 16:17:47 -0400

    changed login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -31,51 +31,30 @@ function Login() {
     }
     
 
-  return (
-    <div>
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='email'>
-                        <strong>Email</strong>
-                    </label>
-                    <input 
-                        type='email'
-                        placeholder='Enter Email'
-                        autoComplete='off'
-                        name='email'
-                        className='email'
-                        onChange={handleInput}
-                    />
-                    {errors.email && <span className='text-danger'> {errors.email}</span>}
-                </div>
-                <div>
-                <label htmlFor='password'>
-                        <strong>Password</strong>
-                    </label>
-                    <input 
-                        type='password'
-                        placeholder='Enter Password'
-                        autoComplete='off'
-                        name='email'
-                        className='email'
-                        onChange={handleInput}
-                    />
-                    {errors.password && <span className='text-danger'> {errors.password}</span>} 
-                </div>
-                <button type='submit' className='subbtn'>
-                    Login
-                </button>
-            </form>
-            <Link to="/">
-                    <button className='btn'>
-                        Register
-                    </button>
-                </Link>
+    return(
+        <div className = "b-flex justify-content-center align-items-center bg-blue">
+            <div className = "bg-white p-3 rounded w-25">
+			<h2>Sign-in</h2>
+                <form action = "" onSubmit = {handleSubmit}>
+                    <div className = "mb-3">
+                        <label htmlFor = "email">Email<strong/></label>
+                        <input type = "email" placeholder = "Enter Email" name='email'
+                        onChange={handleInput} className = 'from-control rounded-0'/>
+                        {errors.email && <span className='text-danger'> {errors.email}</span>}
+
+                    </div>
+                    <div className = "mb-3">
+                        <label htmlFor = "password">Password<strong/></label>
+                        <input type = "password" placeholder = "Enter Password" name ='password'
+                        onChange={handleInput} className = 'from-control rounded-0'/>
+                        {errors.password && <span className='text-danger'> {errors.password}</span>}
+                    </div>
+                    <button type = 'submit' className = "btn btn-success">Login</button>
+                    <Link to = "/Signup" className = "btn btn-default border">Signup</Link>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
