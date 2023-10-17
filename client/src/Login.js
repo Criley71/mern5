ea021f1cbdaf2fad17a968c239ff3f9a6bddd15b
connor <connorriley@outlook.com>
2023-10-17 14:13:07 -0400

    router link

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,22 +6,56 @@ function Login() {
     const [password, setPassword] = useState()
     const nav = useNavigate(); //nav the goat fr
     axios.defaults.withCredentials = true;
-    /*
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         axios.post('https://mern5-api.vercel.app/register', {email, password})
         .then(result => {
             console.log(result)
             if(result.data == "Success"){
-
+                nav('/home')
             }
         })
+        .catch(err => console.log(err))
     }
-    */
+    
 
   return (
     <div>
-        <h1>Login</h1>
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='email'>
+                        <strong>Email</strong>
+                    </label>
+                    <input 
+                        type='email'
+                        placeholder='Enter Email'
+                        autoComplete='off'
+                        name='email'
+                        className='email'
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                <label htmlFor='password'>
+                        <strong>Email</strong>
+                    </label>
+                    <input 
+                        type='password'
+                        placeholder='Enter Password'
+                        autoComplete='off'
+                        name='email'
+                        className='email'
+                        onChange={e => setPassword(e.target.value)}
+                    /> 
+                </div>
+                <button type='submit' className='subbtn'>
+                    Login
+                </button>
+            </form>
+        </div>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -19,19 +19,35 @@ app.get("/", (req, res) => {
     res.json("Hello");
 })
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    RegisterModel.findOne({ email: email })
+        .then(user => {
+            if(user){
+                if(user.password === password){
+                    res.json("Success")
+                }else {
+                    res.json("Incorrect Password")
+                }
+            } else {
+                res.json("No email associated with an account")
+            }
+        })
+})
+
 app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then( user => {
-        if(user) {
-            res.json("already have account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
-        }
-    })
-    .catch(error => res.json(error))
+    const { name, email, password } = req.body;
+    RegisterModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json("already have account")
+            } else {
+                RegisterModel.create({ name: name, email: email, password: password })
+                    .then(result => res.json(result))
+                    .catch(err => res.json(err))
+            }
+        })
+        .catch(error => res.json(error))
 })
 
 app.listen(8000, () => {
