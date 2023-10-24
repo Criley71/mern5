343b4cd3365b55cf1f95ab35015f530767bcd58e
connor <connorriley@outlook.com>
2023-10-24 16:44:50 -0400

    backend

--- client/src/Survey.js
+++ client/src/Survey.js
@@ -4,72 +4,87 @@ import axios from 'axios';
 
 
 function Survey() {
-    const [mealname, setMealName] = useState('');
-    const [mealtime, setMealTime] = useState('');
-    const [insulintime, setInsulinTime] = useState('');
-    const [insulindose, setInsulinDose] = useState('');  
-    const [carbcount, setCarbCount] = useState('');
-   
-  
-    // This is super inefficient but I literally can't find any examples online of how to do this
-    // simpler so I'll keep researching it but if anyone has any tips lmk
-    
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      axios.post('https://mern5-api.vercel.app/survey', {email, mealname})
-        .then()
-    }
+  const [email, setEmail] = useState('')
+  const [mealname, setMealName] = useState('');
+  const [mealtime, setMealTime] = useState('');
+  const [insulintime, setInsulinTime] = useState('');
+  const [insulindose, setInsulinDose] = useState('');
+  const [carbcount, setCarbCount] = useState('');
+  const initialForm = {
+    email: '',
+    mealName: '',
+    mealTime: 0,
+    insulinTime: 0,
+    insulinDose: 0,
+    carbCount: 0
+  }
+  const [form, setForm] = useState(initialForm)
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value })
+  }
 
-    const handleOnSubmit = async (e) => {
-      e.preventDefault();
-      let result = await fetch(
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('https://mern5-api.vercel.app/survey/:_id', { email, form })
+      .then(result => console.log(result))
+      .catch(err => console.log(err))
+  }
+  // This is super inefficient but I literally can't find any examples online of how to do this
+  // simpler so I'll keep researching it but if anyone has any tips lmk
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let result = await fetch(
       'http://localhost:5000/register', {
-          method: "post",
-          body: JSON.stringify({ mealname, mealtime, insulintime, insulindose, carbcount }),
-          headers: {
-              'Content-Type': 'application/json'
-          }
-      })
-      result = await result.json();
-      console.warn(result);
-      if (result) {
-        alert("Data saved succesfully");
-        setMealName("");
-        setMealTime("");
-        setInsulinTime("");
-        setInsulinDose("");
-        setCarbCount("");
+      method: "post",
+      body: JSON.stringify({ mealname, mealtime, insulintime, insulindose, carbcount }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+      setMealName("");
+      setMealTime("");
+      setInsulinTime("");
+      setInsulinDose("");
+      setCarbCount("");
     }
   }
-  
-    console.log(mealname);
-    console.log(mealtime);
-    console.log(insulintime);
-    console.log(Number(insulindose));
-    console.log(Number(carbcount));
+
+  console.log(mealname);
+  console.log(mealtime);
+  console.log(insulintime);
+  console.log(Number(insulindose));
+  console.log(Number(carbcount));
 
   return (
     <main>
       <div>
         <form onSubmit={handleSubmit}>
-            <h2>Meal Survey</h2> <br></br>
+          <h2>Meal Survey</h2> <br></br>
 
-            <h3>Meal Name: </h3>
-            <input type="text" value={mealname} onChange={(e) => setMealName(e.target.value)} id="meal_name" name="meal_name"/>
+          <h3>email</h3>
+          <input type ="text" value={email} onChange={handleChange} id="email" name="email" />
 
-            <h3>Time of Meal: </h3>
-            <input type="time" value={mealtime} onChange={(e) => setMealTime(e.target.value)} name="meal_time"/>
+          <h3>Meal Name: </h3>
+          <input type="text" value={mealName} onChange={handleChange} id="meal_name" name="meal_name" />
 
-            <h3>Time of Insulin Dosage: </h3>
-            <input type="time" value={insulintime} onChange={(e) => setInsulinTime(e.target.value)} name="ins_time"/>
+          <h3>Time of Meal: </h3>
+          <input type="time" value={mealtime} onChange={handleChange} name="meal_time" />
 
-            <h3>Insulin dosage: </h3>
-            <input type="number" value={insulindose} onChange={(e) => setInsulinDose(e.target.value)}/>
+          <h3>Time of Insulin Dosage: </h3>
+          <input type="time" value={insulintime} onChange={handleChange} name="ins_time" />
 
-            <h3>Carb count: </h3>
-            <input type="number" value={carbcount} onChange={(e) => setCarbCount(e.target.value)}/>
+          <h3>Insulin dosage: </h3>
+          <input type="number" value={insulindose} onChange={handleChange} />
 
-            <br></br><button type = "submit">submit</button>
+          <h3>Carb count: </h3>
+          <input type="number" value={carbcount} onChange={handleChange} />
+
+          <br></br><button type="submit">submit</button>
         </form>
       </div>
     </main>
