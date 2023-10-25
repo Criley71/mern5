c034357887bac9228918712aa265fb5d809f27f8
connor <connorriley@outlook.com>
2023-10-25 16:35:04 -0400

    allow

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ app.use(cors(
         credentials: true
     }
 ))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {        
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+     }
+    return next();
+});
+
 app.use(express.json())
 
 mongoose.connect(process.env.MONGODB)
