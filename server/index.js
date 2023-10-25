aff40ced467998ead1efe58cc4e6b29abc681285
connor <connorriley@outlook.com>
2023-10-25 16:00:17 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors(
     {
         origin: ["https://mern5-frontend.vercel.app", "https://mern5-frontend.vercel.app/login", "https://mern5-frontend.vercel.app/home/survey"],
-        methods: ["POST", "GET"],
+        methods: ["POST", "GET", "*"],
         credentials: true
     }
 ))
